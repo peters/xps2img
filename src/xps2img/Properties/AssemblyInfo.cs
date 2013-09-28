88531588e2ac23dd8aa516c03ab935c857289305
Peter Sunde <peter.sunde@gmail.com>
2013-09-28 16:18:39 +0200

    update assembly information

--- src/xps2img/Properties/AssemblyInfo.cs
+++ src/xps2img/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("xps2img")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Convert xps documents to a .NET bitmap")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xps2img")]
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
